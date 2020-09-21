8625f789694649ac1a72000b824b659961123346
Silmar3347 <kumineksilmar@gmail.com>
2020-09-21 12:51:28 -0300

    first commit test

--- ExerciseTest.cs
+++ ExerciseTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace entra21_tests
@@ -18,6 +19,81 @@ namespace entra21_tests
             var result = exercises.Exercise2();
 
             Assert.Equal(5050, result);
+        }
+
+        [Fact]
+        
+        public void Should_return_numbers_from_1_to_200()
+        {
+            var exercises = new Exercises();
+
+            var result = exercises.Exercise3();
+
+            var expectedOutput = new int[]
+            {
+                1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65,67,69,71,73,75,77,79,81,83,85,87,89,91,93,95,97,99,101,103,105,107,109,111,113,115,117,119,121,123,125,127,129,131,133,135,137,139,141,143,145,147,149,151,153,155,157,159,161,163,165,167,169,171,173,175,177,179,181,183,185,187,189,191,193,195,197,199
+            };
+
+            for (int i = 0; i < 100; i++)
+            {
+                Assert.Equal(expectedOutput[i], result[i]);
+            }
+        }
+
+
+        [Fact]
+        public void Should_return_age_average()
+        {
+            var exercises = new Exercises();
+
+            var Mylist = new List<int>(){5,15,40};
+
+            double result = exercises.Exercise4(Mylist);
+
+            Assert.Equal(20, result);
+        }
+
+        [Fact]
+        public void Should_Return_An_Percentage_Women_Between_18_and_35_Years()
+        {
+            //Given
+            var exercises = new Exercises();
+            var womensAges = new int[5]
+            {
+                30,22,25,13,15
+            };
+
+            var result = exercises.Exercise5(womensAges);
+
+            Assert.Equal(60, result);
+
+        }
+
+        [Theory]
+        [InlineData(10,6,2,2190)]
+        [InlineData(20,4.90,44,78694)]
+        public void Should_Return_Money_Spent_With_Cigarretes(double cigarettesPerDay,double walletPrice, double years, double quantity)
+        {
+            var exercises = new Exercises();
+
+            var result = exercises.Exercise7(cigarettesPerDay, walletPrice, years);
+
+            Assert.Equal(quantity, result);
+        }
+
+        [Theory]
+        [InlineData(6, new int[10]{6, 12, 18, 24, 30, 36, 42, 48, 54, 60})]
+        [InlineData(5, new int[10]{5, 10, 15, 20, 25, 30, 35, 40, 45, 50})]
+        public void should_return_the_input_multiplied_by_1_to_10(int number, int[] expectedResult)
+        {
+            // Dado / Setup
+            var exercises = new Exercises();
+            
+            // Quando / Ação
+            var result = exercises.Exercise17(number);
+
+            // Deve / Asserções
+            Assert.Equal(result, expectedResult);
         }
     }
 }
--- Exercises.cs
+++ Exercises.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace entra21_tests
 {
@@ -18,5 +21,72 @@ namespace entra21_tests
             }
             return sum;
         }
-    } 
+
+        public int[] Exercise3()
+        {
+
+            var numbers = new int[100];
+            var count = 1;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = count;
+                count += 2;
+            }
+
+            return numbers;
+        }
+
+        public double Exercise4(List<int> ages)
+        {
+            double sum = 0.0;
+            var a = ages.Count;
+
+            foreach (var item in ages)
+            {
+                sum += item;
+            }
+
+            var average = sum / a;
+
+            return average;
+        }
+
+        public double Exercise5(int[] ages)
+        {
+            var percent = 0.0;
+
+            foreach (var womensAge in ages)
+            {
+                percent = (womensAge > 17 && womensAge < 36) ? percent += 1 : percent;    
+            }
+
+            return Math.Floor((percent/ages.Length)*100);
+        }
+
+        public double Exercise7(double cigarettesPerDay, double walletPrice,  double years)
+        {
+            const int walletCigarettes = 20;
+            var cigarettePrice = walletPrice / walletCigarettes;
+            var daysSmoking = years * 365;
+            return Math.Floor (daysSmoking * cigarettesPerDay * cigarettePrice);
+        }
+
+         public IEnumerable<int> Exercise17(int number)
+		{
+            // Imprimir a tabuada de qualquer número fornecido pelo usuário.
+            // DADO que a aplicação esteja pronta, QUANDO o usuário informar um número
+            // DEVE retornar a tabuada de 1 a 10
+
+            var multiplicationTable = new List<int>(){
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+                                      //  é tipo um foreach,cada vez que
+                                     // \/passa o item(que começa na posição 0)sobe 1 posição.
+                                     // \/ E NAO ALTERA A LISTA (multiplicationTable)
+            return multiplicationTable.Select(item => item * number);
+		}
+    }
+
+     
 }       
